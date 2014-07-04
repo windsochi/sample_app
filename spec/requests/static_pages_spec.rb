920db1b6240799de618255f0edef4f3cae08343c
Litos <windsochi@gmail.com>
2014-07-05 00:03:12 +0400

    lesson 5. Home work

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,11 +4,17 @@ describe "Static pages" do
 
   subject { page }
 
+	shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
@@ -28,7 +34,22 @@ describe "Static pages" do
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
+  end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to 
+    click_link "Contact"
+    expect(page).to 
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to 
+    click_link "sample app"
+    expect(page).to 
   end
 end
